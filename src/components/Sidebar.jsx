3783771e41f0cbd767e001c144de00bcbadf5609
saisandeepkoritala <saisandeep.koritala@gmail.com>
2024-02-07 15:56:27 -0600

    initial commit

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from 'react';
+import {
+    FaTh,
+    FaBars,
+    FaUserAlt,
+    FaRegChartBar,
+    FaCommentAlt,
+    FaShoppingBag,
+    FaThList
+}from "react-icons/fa";
+import { NavLink } from 'react-router-dom';
+import "./sidebar.css";
+
+
+const Sidebar = ({children}) => {
+    const [width,Setwidth]=useState(window.innerWidth);
+
+    useEffect(()=>{
+        const get =()=>{
+            Setwidth(window.innerWidth)
+            if(window.innerWidth<=600){
+                setIsOpen(false)
+            }
+        }
+        window.addEventListener("resize",get)
+        return () => {
+            window.removeEventListener("resize", get);
+        };
+    },[width])
+
+    const[isOpen ,setIsOpen] = useState(true);
+    const toggle = () => setIsOpen (!isOpen);
+    const menuItem=[
+        {
+            path:"/",
+            name:"Dashboard",
+            icon:<FaTh/>
+        },
+        {
+            path:"/about",
+            name:"About",
+            icon:<FaUserAlt/>
+        },
+        {
+            path:"/analytics",
+            name:"Analytics",
+            icon:<FaRegChartBar/>
+        },
+        {
+            path:"/comment",
+            name:"Comment",
+            icon:<FaCommentAlt/>
+        },
+        {
+            path:"/product",
+            name:"Product",
+            icon:<FaShoppingBag/>
+        },
+        {
+            path:"/productList",
+            name:"Product List",
+            icon:<FaThList/>
+        }
+    ]
+    return (
+        <div className="container">
+            <div className={isOpen?"sidebar open":"sidebar close"}>
+                <div className="top_section">
+                    <h1 style={{display: isOpen ? "block" : "none"}} className="logo">Logo</h1>
+                    <div style={{marginLeft: isOpen ? "50px" : "0px"}} className="bars">
+                        <FaBars onClick={!(width<=600)?toggle:""}/>
+                    </div>
+                </div>
+                {
+                    menuItem.map((item, index)=>(
+                        <NavLink to={item.path} key={index} className="link" activeclassName="active">
+                            <div className="icon">{item.icon}</div>
+                            <div style={{display: isOpen ? "block" : "none"}} className="link_text">{item.name}</div>
+                        </NavLink>
+                    ))
+                }
+            </div>
+            <main>{children}</main>
+        </div>
+    );
+};
+
+export default Sidebar;
